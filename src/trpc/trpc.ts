8563f44bd2dcaf8401129f2a8b52f308092d59a4
krutulkamil <krutulkamil@onet.pl>
2024-02-18 11:41:14 +0100

    feat: tRPC create private procedure with isAuth middleware

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,7 +1,27 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from "@trpc/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const t = initTRPC.create();
+
+const middleware = t.middleware;
+
+const isAuthenticated = middleware(async (opts) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user?.id) throw new TRPCError({ code: 'UNAUTHORIZED' });
+
+  return opts.next({
+    ...opts,
+    ctx: {
+      ...opts.ctx,
+      userId: user.id,
+      user,
+    }
+  });
+});
 
 export const router = t.router;
 
 export const publicProcedure = t.procedure;
+export const privateProcedure = t.procedure.use(isAuthenticated);
