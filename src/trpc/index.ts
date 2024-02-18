e2dd265f74a1d3412ff1d23c4adbb75c6d9598a6
krutulkamil <krutulkamil@onet.pl>
2024-02-18 18:42:35 +0100

    feat: delete file private procedure

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,6 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
 
 import { db } from '@/db';
 
@@ -40,6 +41,33 @@ export const appRouter = router({
       },
     });
   }),
+
+  deleteFile: privateProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
