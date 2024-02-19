59fa1104f5148b8988f0619ed47a7d61790f62f1
krutulkamil <krutulkamil@onet.pl>
2024-02-19 14:31:27 +0100

    feat: file page layout init

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { notFound, redirect } from "next/navigation";
+import { notFound, redirect } from 'next/navigation';
 
 import { db } from '@/db';
+import { PdfRenderer } from '@/components/dashboard/pdf-renderer';
+import { ChatWrapper } from '@/components/dashboard/chat-wrapper';
 
 interface FilePageProps {
   params: {
@@ -25,8 +27,18 @@ export default async function FilePage({ params }: Readonly<FilePageProps>) {
   if (!file) notFound();
 
   return (
-    <div>
-      <p>{fileid}</p>
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer />
+          </div>
+        </div>
+
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
     </div>
   );
 }
