8c571939bd9c4f8dd45859726febc9aceb1a0e08
krutulkamil <krutulkamil@onet.pl>
2024-02-20 20:37:47 +0100

    feat: use resize detector for dynamic page width

---
+++ src/components/dashboard/document-loader.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Loader2 } from 'lucide-react';
+
+export const DocumentLoader = () => (
+  <div className="flex justify-center">
+    <Loader2 className="my-24 h-6 w-6 animate-spin" />
+  </div>
+);
--- src/components/dashboard/pdf-renderer.tsx
+++ src/components/dashboard/pdf-renderer.tsx
@@ -2,17 +2,24 @@
 
 import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
+import { useResizeDetector } from 'react-resize-detector';
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
+
+import { useToast } from '@/components/ui/use-toast';
+import { DocumentLoader } from '@/components/dashboard/document-loader';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PdfRendererProps {
   url: string;
 }
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 export function PdfRenderer({ url }: Readonly<PdfRendererProps>) {
+  const { toast } = useToast();
+  const { ref, width } = useResizeDetector();
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
@@ -20,9 +27,20 @@ export function PdfRenderer({ url }: Readonly<PdfRendererProps>) {
       </div>
 
       <div className="flex-1 w-full max-h-screen">
-        <div>
-          <Document file={url} className="max-h-full">
-            <Page pageNumber={1} />
+        <div ref={ref}>
+          <Document
+            file={url}
+            loading={DocumentLoader}
+            onLoadError={(error) =>
+              toast({
+                title: 'Error',
+                description: error.message,
+                variant: 'destructive',
+              })
+            }
+            className="max-h-full"
+          >
+            <Page width={width} pageNumber={1} />
           </Document>
         </div>
       </div>
