1175a020e9031dda26dbe194e25c2d3a1fda79d2
krutulkamil <krutulkamil@onet.pl>
2024-02-24 19:50:36 +0100

    feat: openai message route /w message streaming

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,8 +1,13 @@
 import { NextRequest } from 'next/server';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { PineconeStore } from '@langchain/pinecone';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 import { sendMessageValidator } from '@/schemas/sendMessageValidator';
 import { db } from '@/db';
+import { pinecone } from '@/lib/pinecone';
+import { openai } from '@/lib/openai';
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
@@ -36,4 +41,82 @@ export const POST = async (req: NextRequest) => {
       fileId,
     },
   });
+
+  // vectorize the message
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const pineconeIndex = pinecone.index('reader');
+
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    namespace: file.id,
+  });
+
+  // get results
+  const results = await vectorStore.similaritySearch(message, 4);
+
+  // message history
+  const prevMessages = await db.message.findMany({
+    where: {
+      fileId,
+    },
+    orderBy: {
+      createdAt: 'asc',
+    },
+    take: 6,
+  });
+
+  const formattedMessages = prevMessages.map((message) => ({
+    role: message.isUserMessage ? ('user' as const) : ('assistant' as const),
+    content: message.text,
+  }));
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    temperature: 0,
+    stream: true,
+    messages: [
+      {
+        role: 'system',
+        content:
+          'Use the following pieces of context (or previous conversation if needed) to answer the users question in markdown format.',
+      },
+      {
+        role: 'user',
+        content: `Use the following pieces of context (or previous conversation if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+        
+  \n----------------\n
+  
+  PREVIOUS CONVERSATION:
+  ${formattedMessages.map((message) => {
+    if (message.role === 'user') return `User: ${message.content}\n`;
+    return `Assistant: ${message.content}\n`;
+  })}
+  
+  \n----------------\n
+  
+  CONTEXT:
+  ${results.map((r) => r.pageContent).join('\n\n')}
+  
+  USER INPUT: ${message}`,
+      },
+    ],
+  });
+
+  const stream = OpenAIStream(response, {
+    async onCompletion(completion) {
+      await db.message.create({
+        data: {
+          text: completion,
+          isUserMessage: false,
+          fileId,
+          userId,
+        },
+      });
+    },
+  });
+
+  return new StreamingTextResponse(stream);
 };
